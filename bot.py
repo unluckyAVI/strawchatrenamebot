96e225a177119326bfadcc7416d2d9e29f1b5311
unluckyAVI <unluckyavi588@gmail.com>
2026-07-05 13:31:18 +0530

    .

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def main():
         bot_token=Config.BOT_TOKEN,
         plugins={"root": "handlers"},
         workers=16,
-        max_concurrent_transmissions=3,
+        max_concurrent_transmissions=2,
     )
 
     app.run()
